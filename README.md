bee9787497e8193fd784cceda38aece7d7e9b0c6
safwanjmuhammed <safwanjmuhammed@gmail.com>
2022-11-23 16:50:40 +0530

    nw

---
+++ README.md
@@ -0,0 +1,21 @@
+# Contact_ui
+
+
+![Contact ui](https://user-images.githubusercontent.com/19486593/198878912-c3ea0dd6-0957-4217-8334-7da5b91fc8c9.png)
+
+#movies
+
+![Screenshot (57)](https://user-images.githubusercontent.com/19486593/201029761-38acb9fb-ec5a-45f9-a07e-68bf95dc9a42.png)
+
+#card_ui
+
+
+![Screenshot_20221103_161716](https://user-images.githubusercontent.com/19486593/199702594-ed8a0d30-21d7-49f9-8fa8-aa27608cc27e.png)
+
+#CheckBoxListtileDemo
+
+![Screenshot (56)](https://user-images.githubusercontent.com/19486593/200836536-2a6fad7b-9219-4ab7-87aa-1ffa130f9b5f.png)
+
+#Blog_ui
+
+![blogui](https://user-images.githubusercontent.com/19486593/202638043-82e7823b-387d-42d8-b94e-cebd29fa7085.png)
---
+++ lib/lotteee.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: ListView(
+          children: [
+            // Load a Lottie file from your assets
+            Lottie.asset('assets/animations/loading.json',width: 100,height: 100),
+
+            // Load a Lottie file from a remote url
+            Lottie.network(
+                'https://assets10.lottiefiles.com/packages/lf20_ip8k2zmo.json'),
+
+            // Load an animation and its images from a json file
+            // Lottie.asset('assets/lottiefiles/angel.json'),
+          ],
+        ),
+      ),
+    );
+  }
+}
